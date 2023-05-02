6ad66f0af3a48334b69daf990c03205b36dd39ca
novedice <Natalia.kitesurf@gmail.com>
2023-05-02 17:24:29 +0100

    fix: fix problem with dispatch language

--- graphi-ql/src/components/Layout.tsx
+++ graphi-ql/src/components/Layout.tsx
@@ -3,26 +3,32 @@ import { Link, Outlet } from 'react-router-dom';
 import { useAppDispatch, useTypeSelector } from '../hooks/hooks';
 import Footer from './Footer';
 import { changeLang } from '../store/userInfoSlice';
+import store from '../store/store';
+import { PAGE_STYLE } from '../style-const/style-const';
 
 const Layout = () => {
   const dispatch = useAppDispatch();
-  const lang = useTypeSelector(state => state.user.lang)
+  const { lang, name } = useTypeSelector(state => state.user);
+  const { loggedIn } = useTypeSelector(state => state.login);
+
 
   const handleChangeLang = () => {
-    dispatch(changeLang);
-    console.log(lang);
+    dispatch(changeLang());
   }
   
   return (
     <>
-      <header className="header flex justify-around h-8 w-full bg-gray-700">
-        <Link to="/">Welcome Page</Link>
-        <Link to="/auth">Login/signup</Link>
-        <Link to="/graphi-ql">GraphQL</Link>
-        <div onClick={handleChangeLang}>{lang}</div>
-      </header>
-      <Outlet />
-      <Footer />
+      <div className={`${PAGE_STYLE}`}>
+        <header className="header flex justify-around h-8 w-full bg-gray-700">
+          <Link to="/">Welcome Page</Link>
+          <Link to="/graphi-ql">GraphQL</Link>
+          {!loggedIn && <Link to="/auth">Login/signup</Link>}
+          {loggedIn && <div>{name}</div>}
+          <div onClick={handleChangeLang}>{lang}</div>
+        </header>
+        <Outlet />
+        <Footer />
+      </div>
     </>
   );
 };
--- graphi-ql/src/pages/page404/Page404.tsx
+++ graphi-ql/src/pages/page404/Page404.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
 import { PAGE_STYLE } from "../../style-const/style-const";
 
 const Page404 = () => {
-  return <><div className={`${PAGE_STYLE}`}>Page not found</div></>
+  return <>
+    <div className={`${PAGE_STYLE}`}>
+      <p>Page not found</p>
+      <Link to='/'>
+        <p>Go to Welcome Page</p>
+      </Link>
+    </div>
+  </>
 }
 
 export default Page404;
