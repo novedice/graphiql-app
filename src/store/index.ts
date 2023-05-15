4b74f2d618e5c594ff0bb4da917ff530d3d7d96f
Maksim1509 <maaa23@mail.ru>
2023-05-15 22:37:19 +0400

    refactor: add load schema

---
+++ src/store/index.ts
@@ -0,0 +1,29 @@
+import { configureStore } from '@reduxjs/toolkit';
+import requestValueReducer from './slices/requestSlice';
+import variablesValueReducer from './slices/variablesSlice';
+import resultValueReducer from './slices/resultSlice';
+import loginReducer from './slices/loginSlice';
+import modalWindowReducer from './slices/modalWindowSlice';
+import langReducer from './slices/langSlice';
+import userReducer from './slices/userSlice';
+import variableViewReducer from './slices/variableViewSlice';
+import docSchemaReducer from './slices/docSlice';
+
+const store = configureStore({
+  reducer: {
+    login: loginReducer,
+    modalWindow: modalWindowReducer,
+    requestValue: requestValueReducer,
+    variablesValue: variablesValueReducer,
+    resultValue: resultValueReducer,
+    userLang: langReducer,
+    user: userReducer,
+    variableView: variableViewReducer,
+    docSchema: docSchemaReducer,
+  },
+});
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
