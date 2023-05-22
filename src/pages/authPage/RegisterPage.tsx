7d77cd3a7bcb3fee1117030b4aac1eedad7e0f9c
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2023-05-22 10:55:11 +0200

    refactor: delete unused imports for RegisterPage

--- src/pages/authPage/RegisterPage.tsx
+++ src/pages/authPage/RegisterPage.tsx
@@ -1,6 +1,4 @@
 import Register from '../../components/Auth/Register';
-import { FormattedMessage } from 'react-intl';
-import { Link } from 'react-router-dom';
 
 const RegisterPage = () => {
   return (
